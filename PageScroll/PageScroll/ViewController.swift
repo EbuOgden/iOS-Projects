ee3877d08472e44fd8e7db03ebb41c685bc026a5
Ebubekir <ebubekirogden@gmail.com>
2016-11-18 00:35:26 -0800

    Update

---
+++ PageScroll/PageScroll/ViewController.swift
@@ -0,0 +1,93 @@
+//
+//  ViewController.swift
+//  PageScroll
+//
+//  Created by Ebubekir on 11/17/16.
+//  Copyright © 2016 Ebubekir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var images = [UIImageView]()
+    
+    override func viewDidLoad() {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        
+        let leftSwipe = UISwipeGestureRecognizerDirection.left // Left Swipe Recognizer
+        
+        let rightSwipe = UISwipeGestureRecognizerDirection.right // Right Swipe Recognizer
+        
+        
+        let gestureRecognizerLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.leftSwipeMethod)) // Swipe Recognizer for Left Side!
+        gestureRecognizerLeft.direction = leftSwipe
+        
+        let gestureRecognizerRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.rightSwipeMethod)) // Swipe Recognizer for Right Side!
+        gestureRecognizerRight.direction = rightSwipe
+
+        view.addGestureRecognizer(gestureRecognizerLeft)
+        
+        view.addGestureRecognizer(gestureRecognizerRight)
+        
+        let viewWidth = view.frame.size.width
+        
+        let scrollViewWidth = scrollView.frame.size.width
+        
+        var contentWidth : CGFloat = 0.0
+        
+        for x in 0...2 {
+            
+            let image = UIImage(named: "icon\(x).png")
+            
+            let imageView = UIImageView(image: image)
+            
+            images.append(imageView)
+            
+            var newX : CGFloat = 0.0
+            
+            newX = scrollViewWidth / 2 + scrollViewWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: view.frame.midY - 75, width: 150, height: 150)
+            
+        }
+        
+        contentWidth -= viewWidth
+        
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+
+    }
+    
+    func leftSwipeMethod(recognizer : UISwipeGestureRecognizer){ // Swipe Left Method
+        let locationX = CGFloat(recognizer.location(in: self.view).x)
+        print("Location is : \(locationX)")
+        
+        scrollView.frame.origin.x = locationX
+        
+        
+    }
+    
+    func rightSwipeMethod(){
+        
+    }
+
+
+}
+
